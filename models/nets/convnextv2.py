662a1b6a0e545e3281c48ae259977e24844381d9
JimmyYourHonor <jif055@ucsd.edu>
2025-10-16 22:05:33 -0700

    import fix

--- models/nets/convnextv2.py
+++ models/nets/convnextv2.py
@@ -1,15 +1,15 @@
-from Transformers import ConvNeXtV2Backbone
-from .base import BaseModel
+from transformers import ConvNextV2Backbone
+from .patchnet import BaseNet
 
-class ConvNeXtV2(BaseModel):
+class ConvNeXtV2(BaseNet):
     def __init__(self, model_scale='tiny'):
         super().__init__()
         if model_scale == 'nano':
-            self.backbone = ConvNeXtV2Backbone.from_pretrained("facebook/convnextv2-nano-22k-224")
+            self.backbone = ConvNextV2Backbone.from_pretrained("facebook/convnextv2-nano-22k-224")
         if model_scale == 'tiny':
-            self.backbone = ConvNeXtV2Backbone.from_pretrained("facebook/convnextv2-tiny-22k-224")
+            self.backbone = ConvNextV2Backbone.from_pretrained("facebook/convnextv2-tiny-22k-224")
         elif model_scale == 'base':
-            self.backbone = ConvNeXtV2Backbone.from_pretrained("facebook/convnextv2-base-22k-224")
+            self.backbone = ConvNextV2Backbone.from_pretrained("facebook/convnextv2-base-22k-224")
     def forward_one(self, x):
         output = self.backbone(x)
         return self.normalize(output.feature_maps[-1])
