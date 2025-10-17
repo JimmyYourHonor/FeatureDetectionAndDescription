017c7d9f5dbeca03ccd3000c33c8ca151bb84d2a
JimmyYourHonor <jif055@ucsd.edu>
2025-10-16 22:12:39 -0700

    Model call fix

--- models/nets/convnextv2.py
+++ models/nets/convnextv2.py
@@ -1,5 +1,6 @@
 from transformers import ConvNextV2Backbone
 from .patchnet import BaseNet
+import torch.nn as nn
 
 class ConvNeXtV2(BaseNet):
     def __init__(self, model_scale='tiny'):
@@ -10,6 +11,13 @@ class ConvNeXtV2(BaseNet):
             self.backbone = ConvNextV2Backbone.from_pretrained("facebook/convnextv2-tiny-22k-224")
         elif model_scale == 'base':
             self.backbone = ConvNextV2Backbone.from_pretrained("facebook/convnextv2-base-22k-224")
+        # reliability classifier
+        self.clf = nn.Conv2d(self.out_dim, 2, kernel_size=1)
+        # repeatability classifier: for some reasons it's a softplus, not a softmax!
+        # Why? I guess it's a mistake that was left unnoticed in the code for a long time...
+        self.sal = nn.Conv2d(self.out_dim, 1, kernel_size=1) 
     def forward_one(self, x):
-        output = self.backbone(x)
-        return self.normalize(output.feature_maps[-1])
+        output = self.backbone(x).feature_maps[-1]
+        ureliability = self.clf(output**2)
+        urepeatability = self.sal(output**2)
+        return self.normalize(output, ureliability, urepeatability)
